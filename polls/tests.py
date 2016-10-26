eb108612da562f27c9626a7fb9db97af0ff37342
sonu-dev <sonu.concious@gmail.com>
2016-10-26 22:52:29 +0530

    add first unit test

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,19 @@
+
+from django.utils import timezone
 from django.test import TestCase
+import datetime
+
+from .models import Question
 
 # Create your tests here.
+
+class QuestionMethodTests(TestCase):
+    
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=5)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
