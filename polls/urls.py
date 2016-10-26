e8de9b5b7660671703b4412d960955e27691af93
sonu-dev <sonu.concious@gmail.com>
2016-10-26 22:13:38 +0530

    using generic views

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'polls'
-urlpatterns = [
+"""urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9])/$', views.detail, name='detail'),
@@ -10,4 +10,11 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9])/results/$', views.results, name='results'),
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9])/vote/$', views.vote, name='vote'),
+]"""
+
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
